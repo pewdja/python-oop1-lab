512f3dda390c17b4f59edc9184e8a28018ea5689
pewdja <kimutairoy87@gmail.com>
2026-06-09 20:25:09 +0300

    Your descriptive commit message

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    valid_sizes = ("Small", "Medium", "Large")
+
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if size in self.valid_sizes:
+            self._size = size
+        else:
+            print("size must be Small, Medium, or Large")
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
